9e5503eb0ddb421283b787fe2e8af2b381f0b56a
Nick Hardiman <nhardima@redhat.com>
2023-05-20 15:37:29 +0100

    text

--- machine-hypervisor.sh
+++ machine-hypervisor.sh
@@ -26,6 +26,33 @@ git config --global push.default      simple
 git config --global credential.helper 'cache --timeout=1200'
 git config --global pull.rebase false
 
+# Add an Ansible user account.
+# create a new keypair 
+ssh-keygen -f ./ansible-key -q -N ""
+mv ansible-key  ansible-key.priv
+# Create the ansible_user account (not using --system). 
+useradd ansible_user
+# Copy the keys to ansible_user's SSH config directory. 
+mkdir /home/ansible_user/.ssh
+chmod 0700 /home/ansible_user/.ssh
+cp ansible-key.priv  /home/ansible_user/.ssh/id_rsa
+chmod 0600 /home/ansible_user/.ssh/id_rsa
+cp ansible-key.pub  /home/ansible_user/.ssh/id_rsa.pub
+# enable SSH to localhost with key-based login
+cat ansible-key.pub >> /home/ansible_user/.ssh/authorized_keys
+chmod 0600 /home/ansible_user/.ssh/authorized_keys
+chown -R ansible_user:ansible_user /home/ansible_user/.ssh
+# Allow passwordless sudo.
+echo 'ansible_user      ALL=(ALL)       NOPASSWD: ALL' > /etc/sudoers.d/ansible_user 
+# Copy the keys to your SSH config directory. 
+cp ansible-key.priv  /home/nick/.ssh/
+cp ansible-key.pub  /home/nick/.ssh/
+# Add the location to ansible.cfg. 
+private_key_file = /home/nick/.ssh/ansible-key.priv
+# Check your work. 
+ssh -i /home/nick/.ssh/ansible-key.priv ansible_user@localhost  id
+
+
 # enable nested virtualization? 
 # /etc/modprobe.d/kvm.conf 
 # options kvm_amd nested=1
@@ -38,8 +65,8 @@ cd ~/ansible/collections
 # If the repo has already been cloned, git exits with this error message. 
 #   fatal: destination path 'libvirt-host' already exists and is not an empty directory.
 # !!! not uploaded
-git clone https://github.com/nickhardiman/collection-platform.git
+git clone https://github.com/nickhardiman/ansible-collection-platform.git
 # turn the host into a hypervisor
-# !!! wrong path
+cd ansible-collection-platform
 ansible-playbook --ask-become-pass machine-hypervisor.yml
 
