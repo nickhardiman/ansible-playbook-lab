0c2637b2fe40e9ad4c8410ce50338433f7e0dbe2
Nick Hardiman <nhardima@redhat.com>
2023-05-20 16:02:23 +0100

    simplify gateway

--- machine-hypervisor.sh
+++ machine-hypervisor.sh
@@ -1,7 +1,11 @@
+# !!! make a new role machine-hypervisor-configure.yml
+# and migrate much of this crap there. 
+
 # run
 # log into hypervisor
 # download and run this script
 #   curl -o - https://raw.githubusercontent.com/nickhardiman/homelab/main/machine-hypervisor-configure.sh | bash -x
+
 
 # SSH security
 # if SSH service on this box is accessible to the Internet
@@ -9,13 +13,14 @@
 # for more information, see
 #   man sshd_config
 #echo "AuthenticationMethods publickey" >> /etc/ssh/sshd_config
- 
+
+
 # install Ansible
 sudo dnf install --assumeyes ansible-core
 # install Ansible libvirt collection
 sudo ansible-galaxy collection install community.libvirt --collections-path /usr/share/ansible/collections
 
-# !!! move this to machine-hypervisor-configure.yml
+
 # install git
 sudo dnf install --assumeyes git
 git config --global user.name         "Nick Hardiman"
@@ -25,6 +30,7 @@ git config --global push.default      simple
 # default timeout is 900 seconds (https://git-scm.com/docs/git-credential-cache)
 git config --global credential.helper 'cache --timeout=1200'
 git config --global pull.rebase false
+
 
 # Add an Ansible user account.
 # create a new keypair 
@@ -57,7 +63,8 @@ ssh -i /home/nick/.ssh/ansible-key.priv ansible_user@localhost  id
 # /etc/modprobe.d/kvm.conf 
 # options kvm_amd nested=1
 
-# install my libvirt host role
+
+# get my libvirt collection.
 # I'm not using ansible-galaxy because I am actively developing this role.
 # Check out the directive in ansible.cfg in some playbooks.
 mkdir -p ~/ansible/collections
@@ -66,7 +73,11 @@ cd ~/ansible/collections
 #   fatal: destination path 'libvirt-host' already exists and is not an empty directory.
 # !!! not uploaded
 git clone https://github.com/nickhardiman/ansible-collection-platform.git
+
+
 # turn the host into a hypervisor
-cd ansible-collection-platform
+# get my playbook.
+git clone https://github.com/nickhardiman/ansible-playbook-lab.git
+cd ansible-playbook-lab
 ansible-playbook --ask-become-pass machine-hypervisor.yml
 
