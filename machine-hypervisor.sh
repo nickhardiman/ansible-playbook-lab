5120ea77327b3978d406a90dbdfb0ca7d08079ce
Nick Hardiman <nick@email-domain.com>
2023-07-13 22:23:23 +0100

    shell script and description

--- machine-hypervisor.sh
+++ machine-hypervisor.sh
@@ -3,10 +3,14 @@
 # !!! make a new role machine-hypervisor-configure.yml
 # and migrate much of this crap to that and to collection roles. 
 
-# run
-# log into hypervisor
-# download and run this script
-#   curl -o - https://raw.githubusercontent.com/nickhardiman/ansible-playbook-lab/main/machine-hypervisor.sh | bash -x
+# Log into hypervisor.
+# Download this script.
+#   curl -O https://raw.githubusercontent.com/nickhardiman/ansible-playbook-lab/main/machine-hypervisor.sh 
+# Read it. 
+# Edit and change my details to yours.
+# Set ANSIBLE_GALAXY_SERVER_AUTOMATION_HUB_TOKEN (see below)
+# Run this script
+#   bash -x machine-hypervisor.sh
 
 
 # SSH security
@@ -17,7 +21,7 @@
 #echo "AuthenticationMethods publickey" >> /etc/ssh/sshd_config
 
 
-# install git
+# install and configure git
 sudo dnf install --assumeyes git
 git config --global user.name         "Nick Hardiman"
 git config --global user.email        nick@email-domain.com
@@ -46,15 +50,25 @@ cp ansible-key.pub  /home/ansible_user/.ssh/id_rsa.pub
 cat ansible-key.pub >> /home/ansible_user/.ssh/authorized_keys
 chmod 0600 /home/ansible_user/.ssh/authorized_keys
 chown -R ansible_user:ansible_user /home/ansible_user/.ssh
+# Keep a spare set of keys handy. 
+# Copy the keys to your SSH config directory. 
+cp ansible-key.priv  ansible-key.pub  $HOME/.ssh/
+rm ansible-key.priv  ansible-key.pub
+# This location is set in ansible.cfg. 
+# private_key_file = /home/nick/.ssh/ansible-key.priv
+
+
 # Allow passwordless sudo.
 echo 'ansible_user      ALL=(ALL)       NOPASSWD: ALL' > /etc/sudoers.d/ansible_user 
-# Copy the keys to your SSH config directory. 
-cp ansible-key.priv  /home/nick/.ssh/
-cp ansible-key.pub  /home/nick/.ssh/
-# Add the location to ansible.cfg. 
-private_key_file = /home/nick/.ssh/ansible-key.priv
+
+
 # Check your work. 
-ssh -i /home/nick/.ssh/ansible-key.priv ansible_user@localhost  id
+# Log in with key-based authentication and run the ID command as root.
+ssh \
+     -o StrictHostKeyChecking=no \
+     -i $HOME/.ssh/ansible-key.priv \
+     ansible_user@localhost  \
+     sudo id
 
 
 # enable nested virtualization? 
@@ -74,7 +88,7 @@ ls /usr/share/ansible/collections/ansible_collections/community/
 # I'm not using ansible-galaxy because I am actively developing this role.
 # Check out the directive in ansible.cfg in some playbooks.
 mkdir -p ~/ansible/collections/ansible_collections/nick/
-cd ~/ansible/collections/ansible/collections/ansible_collections/nick/
+cd ~/ansible/collections/ansible_collections/nick/
 # If the repo has already been cloned, git exits with this error message. 
 #   fatal: destination path 'libvirt-host' already exists and is not an empty directory.
 # !!! not uploaded
@@ -87,10 +101,13 @@ git clone https://github.com/nickhardiman/ansible-playbook-lab.git
 cd ansible-playbook-lab
 
 
-# Download Ansible libraries.
+# Authenticate to Red Hat Automation Hub using a token.
 # Get a token from https://console.redhat.com/ansible/automation-hub/token#
 # Set an environment variable.
-export ANSIBLE_GALAXY_SERVER_AUTOMATION_HUB_TOKEN=eyJhbGciOi...
+# export ANSIBLE_GALAXY_SERVER_AUTOMATION_HUB_TOKEN=eyJhbGciOi...
+
+
+# Download Ansible libraries.
 # Install collections. 
 ansible-galaxy collection install -r collections/requirements.yml 
 # Install roles. 
@@ -98,4 +115,5 @@ ansible-galaxy role install -r roles/requirements.yml
 
 
 # set up the QEMU/KVM/libvirt hypervisor
-ansible-playbook --ask-become-pass machine-hypervisor.yml
+# ansible-playbook --ask-become-pass machine-hypervisor.yml
+
